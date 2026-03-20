bd33ae6fa3bba3999a934b66a4a3d2c2789de0b8
yvan <yvan.yang@outlook.com>
2026-03-20 19:56:45 +0800

    Wrap modifier helpers in ModifierFormatting enum
    Move free functions into a caseless enum to avoid polluting the
    module namespace.

--- Sources/Quickey/Models/RecordedShortcut.swift
+++ Sources/Quickey/Models/RecordedShortcut.swift
@@ -2,25 +2,27 @@ import Foundation
 
 // MARK: - Shared modifier helpers
 
-private let hyperModifiers: Set<String> = ["command", "option", "control", "shift"]
+enum ModifierFormatting {
+    static let hyperModifiers: Set<String> = ["command", "option", "control", "shift"]
 
-func modifierSymbol(for modifier: String) -> String {
-    switch modifier.lowercased() {
-    case "command": return "⌘"
-    case "option": return "⌥"
-    case "control": return "⌃"
-    case "shift": return "⇧"
-    case "function": return "fn"
-    default: return modifier
+    static func symbol(for modifier: String) -> String {
+        switch modifier.lowercased() {
+        case "command": return "⌘"
+        case "option": return "⌥"
+        case "control": return "⌃"
+        case "shift": return "⇧"
+        case "function": return "fn"
+        default: return modifier
+        }
     }
-}
 
-func isHyperCombo(_ modifierFlags: [String]) -> Bool {
-    Set(modifierFlags.map { $0.lowercased() }).isSuperset(of: hyperModifiers)
-}
+    static func isHyperCombo(_ modifierFlags: [String]) -> Bool {
+        Set(modifierFlags.map { $0.lowercased() }).isSuperset(of: hyperModifiers)
+    }
 
-func modifierDisplayText(modifierFlags: [String], keyEquivalent: String) -> String {
-    modifierFlags.map(modifierSymbol(for:)).joined() + keyEquivalent.uppercased()
+    static func displayText(modifierFlags: [String], keyEquivalent: String) -> String {
+        modifierFlags.map(symbol(for:)).joined() + keyEquivalent.uppercased()
+    }
 }
 
 // MARK: - RecordedShortcut
@@ -29,13 +31,13 @@ struct RecordedShortcut: Equatable {
     var keyEquivalent: String
     var modifierFlags: [String]
 
-    var isHyper: Bool { isHyperCombo(modifierFlags) }
-    var displayText: String { modifierDisplayText(modifierFlags: modifierFlags, keyEquivalent: keyEquivalent) }
+    var isHyper: Bool { ModifierFormatting.isHyperCombo(modifierFlags) }
+    var displayText: String { ModifierFormatting.displayText(modifierFlags: modifierFlags, keyEquivalent: keyEquivalent) }
 }
 
 // MARK: - AppShortcut display extensions
 
 extension AppShortcut {
-    var isHyper: Bool { isHyperCombo(modifierFlags) }
-    var displayText: String { modifierDisplayText(modifierFlags: modifierFlags, keyEquivalent: keyEquivalent) }
+    var isHyper: Bool { ModifierFormatting.isHyperCombo(modifierFlags) }
+    var displayText: String { ModifierFormatting.displayText(modifierFlags: modifierFlags, keyEquivalent: keyEquivalent) }
 }
